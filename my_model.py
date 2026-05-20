7b0e13010ddb117fcae839711a2f848575fd1663
CongkeChen <is.chris.chen@gmail.com>
2026-05-20 14:38:27 -0700

    Update vocab_size in my_model.py

--- my_model.py
+++ my_model.py
@@ -203,7 +203,8 @@ class Block_abt2(nn.Module):
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    # vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    vocab_size: int = 50257
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
