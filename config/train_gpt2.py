c5380048689a7f63c2cd20548d1f91cbcdaa7e56
enderturtleorz <enderturtle@foxmail.com>
2026-05-19 23:49:25 -0700

    feat: modify train_gpt2.py and upload colab notebook

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -1,50 +1,38 @@
-# config for milestone-scale pretraining on FineWeb-Edu.
-# Intended for a single Colab A100 80GB run:
-# $ python train.py config/train_gpt2.py
 import time
 
-# these make the total batch size be ~0.4M tokens
-# 12 batch size * 1024 block size * 33 gradaccum = 405,504
-batch_size = 12
-block_size = 1024
-gradient_accumulation_steps = 33
-
-# this makes total number of tokens be ~2.03B
-# 12 * 1024 * 33 * 5000 = 2,027,520,000
-max_iters = 5000
-lr_decay_iters = 5000
-
-# eval stuff
+out_dir = f"nano_fineweb_edu_{time.strftime('%m%d_%H%M%S')}"
 eval_interval = 250
 eval_iters = 50
 log_interval = 10
 
-# weight decay
-weight_decay = 1e-1
-learning_rate = 3e-4
-min_lr = 3e-5
-warmup_iters = 200
+always_save_checkpoint = False
 
-# dataset
+wandb_log = True
+wandb_project = '251B'
+wandb_group_name = 'fineweb_edu'
+wandb_run_name = out_dir
+
 dataset = 'fineweb_edu'
+gradient_accumulation_steps = 33
+batch_size = 12
+block_size = 1024
 
-# model
 model_name = 'nano'
-# total params are ~98.4M with vocab_size=50304 and block_size=1024
 n_layer = 10
 n_head = 10
 n_embd = 640
+dropout = 0.0
 
-# model_name = 'gpt2'
-# n_layer = 9
-# n_head = 12
-# n_embd = 672
+# adamw optimizer
+learning_rate = 3e-4
+max_iters = 5000
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0
 
-out_dir = f"{model_name}_{time.strftime('%m%d_%H%M%S')}"
-
-# wandb
-wandb_log = True
-wandb_project = 'milestone_large'
-wandb_run_name = f"{model_name}_{time.strftime('%m%d_%H%M%S')}"
-
-# python train.py config/train_gpt2.py
+# learning rate decay settings
+decay_lr = True
+warmup_iters = 200
+lr_decay_iters = 5000
+min_lr = 3e-5
